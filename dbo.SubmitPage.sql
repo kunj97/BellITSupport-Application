92f3ebaf8bf2ab969df20400eacb2edd7f839453
kunj97 <36387959+kunj97@users.noreply.github.com>
2020-05-11 19:22:16 -0400

    Final Push
    Relse

---
+++ dbo.SubmitPage.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SubmitPage] (
+    [ProjectName]    VARCHAR (50)   NULL,
+    [DepartmentName] VARCHAR (50)   NULL,
+    [FirstName]      VARCHAR (50)   NULL,
+    [LastName]       VARCHAR (50)   NULL,
+    [DOP]            VARCHAR (2500) NULL
+);
+
